cab990f1609d560f90e1805f3c4fe4fa9eb2a923
AshCastilloC <49240474+AshCastilloC@users.noreply.github.com>
2019-04-03 08:40:06 -0500

    Create L01.cpp

---
+++ Basics/L01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Conversiones de tipos no seguras
+    double d = 0;
+    while (cin >> d){
+        int i = d;
+        char c = i;
+        int i2 = c;
+        cout << "d==" << d
+            << "_i=="   << i
+            << "_i2=="  << i2
+            << "_char(" <<  c   << ")\n";
+    }
+    double x = 2.7;
+    int y = x; // y se convierte en 2
+    int a = 1000;
+    char b = a; // b se convierte en -24
+}
