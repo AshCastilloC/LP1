0914aed345c95c55ab648df6b426eaaadf202868
AshCastilloC <49240474+AshCastilloC@users.noreply.github.com>
2019-05-03 12:58:18 -0500

    Create laboratorio_05.cpp

---
+++ Basics2/laboratorio_05.cpp
@@ -0,0 +1,9 @@
+int area ( int length , int width ) 
+{ 
+	return length * width ; 
+}
+const int frame width = 2;
+ // Calcula el area dentro de un marco int framed area(int x, int y) 
+{ 
+	return area(x-frame width ,y-frame width ); 
+} 
