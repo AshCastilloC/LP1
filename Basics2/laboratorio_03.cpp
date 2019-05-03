fa5a78f75e0e852a8bd74583158b1823c7836cb7
AshCastilloC <49240474+AshCastilloC@users.noreply.github.com>
2019-05-03 13:26:06 -0500

    Update laboratorio_03.cpp

--- Basics2/laboratorio_03.cpp
+++ Basics2/laboratorio_03.cpp
@@ -1,12 +1,14 @@
-int area ( int length , int width );
-int main() 
-{ 
-	int x4 = area(10, -7); // ok: pero rectangulo // con ancho -7? 
-	int x5 = area(10.7,9.3);// ok: pero llama a // area (10 ,9) 
-	char x6 = area(100, 9999);// ok, pero trunca // el resultado
-	return area (4 ,4); 
+#include <iostream>
+using namespace std;
+int area(int length , int width);
+int main ()
+{
+  int x4=area(10,7) ; 
+  int x5 = area(1,9 ) ; 
+  int x6 = area( 1, 9) ; 
+  return area ( 4 , 4 ) ;
 }
- int area ( int length , int width ) 
-{ 
-	return length * width ; 
-} 
+int area(int length, int width)
+{
+  return length*width ;
+}
