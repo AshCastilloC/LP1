063ca980bfb1ba15f0eacaaf67405c56e34c21cc
AshCastilloC <49240474+AshCastilloC@users.noreply.github.com>
2019-05-03 13:23:45 -0500

    Update laboratorio_01.cpp

--- Basics2/laboratorio_01.cpp
+++ Basics2/laboratorio_01.cpp
@@ -1,13 +1,15 @@
-int area ( int length , int width ); 
-int main() 
-{ 
-int s1 = area(7; // error: falta ) 
-int s1 = area(7) // error: falta ; 
-Int s3 = area(7); // error: Int no es un tipo 
-int s4 = area(’7); // error: caracter ( falta’) 
-return area (4 ,4); 
+#include <iostream> 
+using namespace std; 
+//int area ( int length, int width );
+int area ( int length, int width )
+{
+   return length*width ;
 }
-int area ( int length , int width ) 
-{ 
-  return length * width ;
- } 
+int main ( )
+{
+   int s1 =area(7,7) ;//(7);
+   int s2 =area (7,7) ;//( 7 ) 
+   int s3 =area (7,7) ; //(7); 
+   int s4 =area (7,7) ; //() ;
+   return area ( 4 , 4 ) ;
+}
