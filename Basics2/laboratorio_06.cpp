a3c370b86a8b50e26f0cdfc9371325ce146c8f1e
AshCastilloC <49240474+AshCastilloC@users.noreply.github.com>
2019-05-03 13:07:38 -0500

    Create laboratorio_06.cpp

---
+++ Basics2/laboratorio_06.cpp
@@ -0,0 +1,11 @@
+int main()
+try 
+{ 
+	int x = 1; 
+	int y = 2; 
+ { 
+	if (x<=0) throw runtime error(”x es negativo”); 
+	if (y<=0) throw runtime error(”y es negativo”); 
+	int area1 = area(x,y); 
+ }
+}
